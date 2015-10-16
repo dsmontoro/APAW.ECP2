d9f4cd6a4f699fc9036b6c7b6e16c17237199392
dsmontoro <dsm@alumnos.upm.es>
2015-10-16 20:50:47 +0200

    Cambios en apaw.ecp2

---
+++ src/main/java/presentation/frontController/Dispatcher.java
@@ -0,0 +1,5 @@
+package presentation.frontController;
+
+public class Dispatcher {
+
+}
---
+++ src/main/java/presentation/frontController/FrontController.java
@@ -0,0 +1,18 @@
+package presentation.frontController;
+
+public class FrontController {
+    private Dispatcher dispatcher;
+
+    public FrontController() {
+        dispatcher = new Dispatcher();
+    }
+
+    public void doGet(HttpRequest request, HttpResponse response) {
+        dispatcher.doGet(request, response);
+    }
+    
+    public void doPost(HttpRequest request, HttpResponse response) {
+        dispatcher.doPost(request, response);
+    }
+
+}
