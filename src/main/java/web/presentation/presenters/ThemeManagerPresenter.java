262b2f23beb3f06cc6b65953bdaf81c7fbcae56e
dsmontoro <dsm@alumnos.upm.es>
2015-10-19 20:13:09 +0200

    Cambios en los Presenters

--- src/main/java/web/presentation/presenters/ThemeManagerPresenter.java
+++ src/main/java/web/presentation/presenters/ThemeManagerPresenter.java
@@ -1,5 +1,21 @@
 package web.presentation.presenters;
 
+import web.presentation.models.Model;
+
 public class ThemeManagerPresenter {
+
+    private String themeName;
+    
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+    
+    public String process(Model model) {
+        return "ThemeManagerView";
+    }
+
+    public String createTheme(Model model) {
+        return "ThemeManagerView";
+    }
 
 }
--- src/main/java/web/presentation/presenters/VotingPresenter.java
+++ src/main/java/web/presentation/presenters/VotingPresenter.java
@@ -1,5 +1,27 @@
 package web.presentation.presenters;
 
+import web.presentation.models.Model;
+
 public class VotingPresenter {
+
+    private String themeName;
+    
+    private Integer value;
+    
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;        
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;        
+    }
+    
+    public String process(Model model) {
+        return "VotingView";
+    }
+
+    public String voteTheme(Model model) {
+        return "VotingView";
+    }
 
 }
