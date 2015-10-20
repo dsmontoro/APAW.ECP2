01a1e087b1a73bb5ae49269137acbdad0a869410
dsmontoro <dsm@alumnos.upm.es>
2015-10-20 19:21:32 +0200

    Cambios en ThemeManagerPresenter

--- src/main/java/web/presentation/presenters/ThemeManagerPresenter.java
+++ src/main/java/web/presentation/presenters/ThemeManagerPresenter.java
@@ -11,20 +11,18 @@ public class ThemeManagerPresenter {
 
     private String themeName;
     
+    private ArrayList<Theme> themes = (ArrayList<Theme>) DaoFactory.getFactory().getThemeDao().findAll();
+    
     public void setThemeName(String themeName) {
         this.themeName = themeName;
     }
     
-    public String process(Model model) {
-        List<Theme> themes = new ArrayList<>();
-    	themes = DaoFactory.getFactory().getThemeDao().findAll();
+    public String process(Model model) {        
     	model.put("themes", themes);
         return "ThemeManagerView";
     }
 
-    public String createTheme(Model model) {  
-        List<Theme> themes = new ArrayList<>();
-        themes = DaoFactory.getFactory().getThemeDao().findAll();
+    public String createTheme(Model model) {        
     	Theme theme = new Theme(themes.size(),themeName);
     	DaoFactory.getFactory().getThemeDao().create(theme);
     	themes.add(theme);
