4b38adda029baf2cf44f6687a7a816f0449bdbae
dsmontoro <dsm@alumnos.upm.es>
2015-10-20 15:53:23 +0200

    Cambio en ThemeManagerView

--- src/main/java/web/presentation/views/ThemeManagerView.java
+++ src/main/java/web/presentation/views/ThemeManagerView.java
@@ -1,5 +1,9 @@
 package web.presentation.views;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import rest.business.models.entities.Theme;
 import web.presentation.models.Model;
 
 public class ThemeManagerView implements View {
@@ -7,7 +11,15 @@ public class ThemeManagerView implements View {
 	@Override
 	public void show(Model model) {
 	    System.out.println("Theme Manager Page");
-	    System.out.println("   Temas: ");
+	    System.out.print("   Temas: [");
+	    List<Theme> themes = (ArrayList<Theme>)model.get("themes");
+	    for (int i=0 ; i<themes.size() ; i++){
+	    	if (i==0)
+	    		System.out.print(themes.get(i).getName());
+	    	else
+	    		System.out.print(", " + themes.get(i).getName());
+	    }
+	    System.out.println("]");
 	}
 
 }
